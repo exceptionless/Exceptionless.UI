1a72634287039232f0edb090303265f1d7c100cb
Blake Niemyjski <bniemyjski@gmail.com>
2014-11-25 11:10:38 -0600

    Added the ability to throttle notifications.

--- src/components/refresh-on/refresh-on-directive.js
+++ src/components/refresh-on/refresh-on-directive.js
@@ -1,41 +1,49 @@
 (function () {
   'use strict';
 
-  angular.module('exceptionless.refresh', ['debounce'])
-    .directive('refreshOn', ['$parse', '$rootScope', 'debounce', function ($parse, $rootScope, debounce) {
-      return {
-        restrict: 'AE',
-        link: function (scope, element, attrs) {
-          function runActionOnEvent(name, action, refreshIf) {
-            var unbind = $rootScope.$on(name, function (event, data) {
-              if (refreshIf && !scope.$eval(refreshIf)) {
-                return;
-              }
+  angular.module('exceptionless.refresh', [
+    'debounce',
+    'exceptionless.throttle'
+  ])
+  .directive('refreshOn', ['$parse', '$rootScope', 'debounce', 'throttle', function ($parse, $rootScope, debounce, throttle) {
+    return {
+      restrict: 'AE',
+      link: function (scope, element, attrs) {
+        function runActionOnEvent(name, action, refreshIf) {
+          var unbind = $rootScope.$on(name, function (event, data) {
+            if (refreshIf && !scope.$eval(refreshIf)) {
+              return;
+            }
 
-              action(scope, data);
-            });
+            action(scope, data);
+          });
 
-            scope.$on('$destroy', unbind);
-          }
-
-          if (!attrs.refreshAction) {
-            return;
-          }
-
-          var action = attrs.refreshDebounce ? debounce($parse(attrs.refreshAction), attrs.refreshDebounce, true) : $parse(attrs.refreshAction);
-
-          if (attrs.refreshOn) {
-            angular.forEach(attrs.refreshOn.split(" "), function (name) {
-              runActionOnEvent(name, action, attrs.refreshIf);
-            });
-          }
-
-          if (attrs.refreshAlways) {
-            angular.forEach(attrs.refreshAlways.split(" "), function (name) {
-              runActionOnEvent(name, action);
-            });
-          }
+          scope.$on('$destroy', unbind);
         }
-      };
-    }]);
+
+        if (!attrs.refreshAction) {
+          return;
+        }
+
+        var action = $parse(attrs.refreshAction);
+        if (attrs.refreshDebounce) {
+          action = debounce(action, attrs.refreshDebounce, true);
+        } else if (attrs.refreshThrottle) {
+          action = throttle(action, attrs.refreshThrottle, true);
+        }
+
+        if (attrs.refreshOn) {
+          angular.forEach(attrs.refreshOn.split(" "), function (name) {
+            runActionOnEvent(name, action, attrs.refreshIf);
+          });
+        }
+
+        if (attrs.refreshAlways) {
+          angular.forEach(attrs.refreshAlways.split(" "), function (name) {
+            runActionOnEvent(name, action);
+          });
+        }
+      }
+    };
+  }]);
 }());
