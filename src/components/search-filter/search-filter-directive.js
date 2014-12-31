0ceff23ae25a5fef5d8e8ff4ff7a79abef1d1f93
Blake Niemyjski <bniemyjski@gmail.com>
2014-12-31 09:10:49 -0600

    Clear Search Filter isn't resetting mark fixed / mark hidden

--- src/components/search-filter/search-filter-directive.js
+++ src/components/search-filter/search-filter-directive.js
@@ -1,48 +1,57 @@
 (function () {
   'use strict';
 
-  angular.module('exceptionless.search-filter', [])
-    .directive('searchFilter', function () {
-      return {
-        restrict: 'E',
-        replace: true,
-        scope: true,
-        templateUrl: 'components/search-filter/search-filter-directive.tpl.html',
-        controller: ['filterService', function (filterService) {
-          var vm = this;
+  angular.module('exceptionless.search-filter', [
+    'exceptionless.refresh'
+  ])
+  .directive('searchFilter', function () {
+    return {
+      restrict: 'E',
+      replace: true,
+      scope: true,
+      templateUrl: 'components/search-filter/search-filter-directive.tpl.html',
+      controller: ['filterService', function (filterService) {
+        var vm = this;
 
-          function clearFilter() {
-            filterService.clearFilterAndIncludeFixedAndIncludeHidden();
-            vm.isDropDownOpen = !vm.isDropDownOpen;
-          }
+        function clearFilter() {
+          filterService.clearFilterAndIncludeFixedAndIncludeHidden();
+          vm.isDropDownOpen = !vm.isDropDownOpen;
+        }
 
-          function hasFilter() {
-            return filterService.getFilter() || filterService.getIncludeFixed() || filterService.getIncludeHidden();
-          }
+        function hasFilter() {
+          return filterService.getFilter() || filterService.getIncludeFixed() || filterService.getIncludeHidden();
+        }
 
-          function setFilter(filter) {
-            filterService.setFilter(filter);
-          }
+        function setFilter(filter) {
+          filterService.setFilter(filter);
+        }
 
-          function setIncludeFixed(includeFixed) {
-            filterService.setIncludeFixed(includeFixed);
-          }
+        function setIncludeFixed(includeFixed) {
+          filterService.setIncludeFixed(includeFixed);
+        }
 
-          function setIncludeHidden(includeHidden) {
-            filterService.setIncludeHidden(includeHidden);
-          }
+        function setIncludeHidden(includeHidden) {
+          filterService.setIncludeHidden(includeHidden);
+        }
 
-          vm.clearFilter = clearFilter;
-          vm.hasFilter = hasFilter;
+        function updateFilter() {
           vm.filter = filterService.getFilter();
-          vm.includeFixed = filterService.getIncludeFixed();
+          vm.includeFixed =  filterService.getIncludeFixed();
           vm.includeHidden = filterService.getIncludeHidden();
-          vm.isDropDownOpen = false;
-          vm.setFilter = setFilter;
-          vm.setIncludeFixed = setIncludeFixed;
-          vm.setIncludeHidden = setIncludeHidden;
-        }],
-        controllerAs: 'vm'
-      };
-    });
+        }
+
+        vm.clearFilter = clearFilter;
+        vm.filter = filterService.getFilter();
+        vm.hasFilter = hasFilter;
+        vm.isDropDownOpen = false;
+        vm.includeFixed =  filterService.getIncludeFixed();
+        vm.includeHidden = filterService.getIncludeHidden();
+        vm.setFilter = setFilter;
+        vm.setIncludeFixed = setIncludeFixed;
+        vm.setIncludeHidden = setIncludeHidden;
+        vm.updateFilter = updateFilter;
+      }],
+      controllerAs: 'vm'
+    };
+  });
 }());
