0156ed3f31a94c38c53956d8f5acf52322296d55
Blake Niemyjski <bniemyjski@gmail.com>
2014-12-31 08:51:30 -0600

    Updates to auth.

--- src/app/auth/auth.js
+++ src/app/auth/auth.js
@@ -4,8 +4,10 @@
   angular.module('app.auth', [
     'directives.inputMatch',
     'ngMessages',
+    'satellizer',
     'ui.router',
 
+    'app.config',
     'exceptionless.auth',
     'exceptionless.notification',
     'exceptionless.project',
@@ -13,7 +15,34 @@
     'exceptionless.user',
     'exceptionless.validators'
   ])
-  .config(['$stateProvider', function ($stateProvider) {
+  .config(['$authProvider', '$stateProvider', 'BASE_URL', 'FACEBOOK_APPID', 'GOOGLE_APPID', 'GITHUB_APPID', 'LIVE_APPID', function ($authProvider, $stateProvider, BASE_URL, FACEBOOK_APPID, GOOGLE_APPID, GITHUB_APPID, LIVE_APPID) {
+    $authProvider.loginUrl = BASE_URL + '/auth/login';
+    $authProvider.loginRedirect = false;
+    $authProvider.logoutRedirect = '/login';
+    $authProvider.signupUrl = BASE_URL + '/auth/signup';
+    $authProvider.signupRedirect = false;
+    $authProvider.unlinkUrl = BASE_URL + '/auth/unlink/';
+
+    $authProvider.facebook({
+      clientId: FACEBOOK_APPID,
+      url: BASE_URL + '/auth/facebook'
+    });
+
+    $authProvider.google({
+      clientId: GOOGLE_APPID,
+      url: BASE_URL + '/auth/google'
+    });
+
+    $authProvider.github({
+      clientId: GITHUB_APPID,
+      url: BASE_URL + '/auth/github'
+    });
+
+    $authProvider.live({
+      clientId: LIVE_APPID,
+      url: BASE_URL + '/auth/live'
+    });
+
     $stateProvider.state('auth', {
       abstract: true,
       template: '<ui-view autoscroll="true" />'
--- src/components/auth/auth-service.js
+++ src/components/auth/auth-service.js
@@ -1,84 +1,89 @@
 (function () {
   'use strict';
 
-  angular.module('exceptionless.auth')
-    .factory('authService', ['$auth', '$location', '$rootScope', '$state', 'stateService', 'Restangular', function ($auth, $location, $rootScope, $state, stateService, Restangular) {
-      function authenticate(provider) {
-        function onSuccess() {
-          $rootScope.$emit('auth:login', {});
-        }
+  angular.module('exceptionless.auth', [
+    'restangular',
+    'satellizer',
 
-        return $auth.authenticate(provider).then(onSuccess);
+    'exceptionless.state'
+  ])
+  .factory('authService', ['$auth', '$rootScope', 'stateService', 'Restangular', function ($auth, $rootScope, stateService, Restangular) {
+    function authenticate(provider) {
+      function onSuccess() {
+        $rootScope.$emit('auth:login', {});
       }
 
-      function changePassword(changePasswordModel) {
-        return Restangular.one('auth', 'change-password').customPOST(changePasswordModel);
+      return $auth.authenticate(provider).then(onSuccess);
+    }
+
+    function changePassword(changePasswordModel) {
+      return Restangular.one('auth', 'change-password').customPOST(changePasswordModel);
+    }
+
+    function forgotPassword(email) {
+      return Restangular.one('auth', 'forgot-password').one(email).get();
+    }
+
+    function getToken() {
+      return $auth.getToken();
+    }
+
+    function isAuthenticated() {
+      return $auth.isAuthenticated();
+    }
+
+    function isEmailAddressAvailable(email) {
+      return Restangular.one('auth', 'check-email-address').one(email).get();
+    }
+
+    function login(user) {
+      function onSuccess() {
+        $rootScope.$emit('auth:login', {});
       }
 
-      function forgotPassword(email) {
-        return Restangular.one('auth', 'forgot-password').one(email).get();
+      return $auth.login(user).then(onSuccess);
+    }
+
+    function logout() {
+      function onSuccess() {
+        $rootScope.$emit('auth:logout', {});
       }
 
-      function getToken() {
-        return $auth.getToken();
-      }
+      stateService.clear();
+      return $auth.logout().then(onSuccess);
+    }
 
-      function isAuthenticated() {
-        return $auth.isAuthenticated();
-      }
+    function resetPassword(resetPasswordModel) {
+      return Restangular.one('auth', 'reset-password').customPOST(resetPasswordModel);
+    }
 
-      function isEmailAddressAvailable(email) {
-        return Restangular.one('auth', 'check-email-address').one(email).get();
-      }
+    function signup(user) {
+      return $auth.signup(user);
+    }
 
-      function login(user) {
-        function onSuccess() {
-          $rootScope.$emit('auth:login', {});
-        }
+    function unlink(providerName, providerUserId) {
+      return $auth.unlink(providerName + '/' + providerUserId);
+    }
 
-        return $auth.login(user).then(onSuccess);
-      }
+    function verifyEmailAddress(token) {
+      return Restangular.one('auth', 'verify-email-address').one(token).get();
+    }
 
-      function logout() {
-        function onSuccess() {
-          $rootScope.$emit('auth:logout', {});
-        }
+    var service = {
+      authenticate: authenticate,
+      changePassword: changePassword,
+      forgotPassword: forgotPassword,
+      getToken: getToken,
+      isAuthenticated: isAuthenticated,
+      isEmailAddressAvailable: isEmailAddressAvailable,
+      login: login,
+      logout: logout,
+      resetPassword: resetPassword,
+      signup: signup,
+      unlink: unlink,
+      verifyEmailAddress: verifyEmailAddress
+    };
 
-        stateService.clear();
-        return $auth.logout().then(onSuccess);
-      }
-
-      function resetPassword(resetPasswordModel) {
-        return Restangular.one('auth', 'reset-password').customPOST(resetPasswordModel);
-      }
-
-      function signup(user) {
-        return $auth.signup(user);
-      }
-
-      function unlink(providerName, providerUserId) {
-        return $auth.unlink(providerName + '/' + providerUserId);
-      }
-
-      function verifyEmailAddress(token) {
-        return Restangular.one('auth', 'verify-email-address').one(token).get();
-      }
-
-      var service = {
-        authenticate: authenticate,
-        changePassword: changePassword,
-        forgotPassword: forgotPassword,
-        getToken: getToken,
-        isAuthenticated: isAuthenticated,
-        isEmailAddressAvailable: isEmailAddressAvailable,
-        login: login,
-        logout: logout,
-        resetPassword: resetPassword,
-        signup: signup,
-        unlink: unlink,
-        verifyEmailAddress: verifyEmailAddress
-      };
-
-      return service;
-    }]);
+    return service;
+  }]);
 }());
