00d62d320f739131c3d4a3dcabd9202427dd3821
Eric J. Smith <eric@codesmithtools.com>
2015-01-13 15:44:39 -0600

    Fix bad JS.

--- src/components/admin/admin-service.js
+++ src/components/admin/admin-service.js
@@ -4,7 +4,6 @@
   angular.module('exceptionless.admin', ['restangular'])
     .factory('adminService', ['Restangular', function (Restangular) {
       function changePlan(options) {
-        options.organization
         return Restangular.one('admin').customPOST(null, 'change-plan', options);
       }
 
