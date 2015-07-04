7510ea8f3edb7fd62f6eb0d57de6e2fd2c7cc03e
Eric J. Smith <eric@codesmithtools.com>
2015-07-03 21:36:54 -0500

    Add custom deployment files to the artifact.

--- src/grunt/task-configs/copy.js
+++ src/grunt/task-configs/copy.js
@@ -1,7 +1,8 @@
 module.exports = {
     main: {
         files: [
-            {src: ['app_data/**', 'app.config.js', 'web.config', '*.png', '*.ico', 'img/**/{*.png,*.jpg,*.ico}'], dest: 'dist/'}
+            { src: [ 'app_data/**', 'app.config.js', 'web.config', '*.png', '*.ico', 'img/**/{*.png,*.jpg,*.ico}' ], dest: 'dist/' },
+            { src: [ '../.deployment', '../deploy.sh' ], dest: 'dist/blah/' }
         ]
     }
 };
