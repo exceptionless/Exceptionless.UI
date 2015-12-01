7cb96c811830f1e32fe7809d33767ae6ff03478c
Blake Niemyjski <bniemyjski@gmail.com>
2015-12-01 14:31:42 -0600

    Updates to Get By Reference Id based on feedback

--- src/app/event/reference-controller.js
+++ src/app/event/reference-controller.js
@@ -2,26 +2,20 @@
   'use strict';
 
   angular.module('app.event')
-    .controller('Reference', ['eventService', 'notificationService', '$state', '$stateParams', function (eventService, notificationService, $state, $stateParams) {
+    .controller('Reference', ['eventService', '$state', '$stateParams', function (eventService, $state, $stateParams) {
       var vm = this;
-      var _referenceId = $stateParams.referenceId;
-
-      function getByReferenceId(options) {
-        return eventService.getByReferenceId(_referenceId, options).then(function(response) {
-          var events = response.data.plain();
-          if (events.length === 0) {
-            $state.go('app.dashboard');
-            notificationService.error('No events with reference id "' + _referenceId + '" could not be found.');
-          } else if (events.length === 1) {
-            $state.go('app.event', { id: events[0].id });
-          }
-
-          return response;
-        });
-      }
 
       vm.references = {
-        get: getByReferenceId,
+        get: function(options) {
+          return eventService.getByReferenceId($stateParams.referenceId, options).then(function(response) {
+            var events = response.data.plain();
+            if (events && events.length === 1) {
+              $state.go('app.event', { id: events[0].id });
+            }
+
+            return response;
+          });
+        },
         options: {
           limit: 20,
           mode: 'summary'
