f5d6ae5c7a1a2cf32a87339a4d035b9c4f3fd54c
Blake Niemyjski <bniemyjski@gmail.com>
2016-03-08 18:29:20 -0600

    publish pull requests

--- src/grunt/task-configs/gh-pages.js
+++ src/grunt/task-configs/gh-pages.js
@@ -1,14 +1,14 @@
 /* jslint node: true */
 module.exports = function (grunt) {
+  var branch = process.env.APPVEYOR_REPO_BRANCH || 'master';
   if (process.env.APPVEYOR_PULL_REQUEST_NUMBER) {
-    grunt.log.writeln('Artifacts will not be created for pull requests.');
-    return;
+    branch = 'pr-' + process.env.APPVEYOR_PULL_REQUEST_NUMBER;
   }
 
   return {
     options: {
       base: 'dist',
-      branch: process.env.APPVEYOR_REPO_BRANCH || 'master',
+      branch: branch,
       message: 'Build: ' + process.env.APPVEYOR_BUILD_VERSION + ' Author: ' + process.env.APPVEYOR_REPO_COMMIT_AUTHOR + ' ' + process.env.APPVEYOR_REPO_NAME + '@' + process.env.APPVEYOR_REPO_COMMIT,
       repo: process.env.BUILD_REPO_URL,
       silent: true,
