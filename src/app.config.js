eaa9e31a83d84dec22c3f176bdb7dbb154fc18af
Blake Niemyjski <bniemyjski@gmail.com>
2016-03-29 18:16:21 -0500

    Removed the default debug exceptionless api key.
    this just made it hard to debug server side stuff with an invalid key
    being sent every few seconds.

--- src/app.config.js
+++ src/app.config.js
@@ -3,7 +3,7 @@
 
   angular.module('app.config', [])
     .constant('BASE_URL', 'http://localhost:50000')
-    .constant('EXCEPTIONLESS_API_KEY', 'Bx7JgglstPG544R34Tw9T7RlCed3OIwtYXVeyhT2')
+    .constant('EXCEPTIONLESS_API_KEY')
     .constant('FACEBOOK_APPID')
     .constant('GITHUB_APPID')
     .constant('GOOGLE_APPID')
