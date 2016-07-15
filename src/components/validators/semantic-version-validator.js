d89e80cbd75ab11da49764f7c4791a6b59efefc7
Blake Niemyjski <bniemyjski@gmail.com>
2016-07-15 10:55:09 -0500

    Updates to the mark fixed dialog

--- src/components/validators/semantic-version-validator.js
+++ src/components/validators/semantic-version-validator.js
@@ -8,12 +8,21 @@
         require: 'ngModel',
         link: function(scope, element, attrs, ngModel) {
           ngModel.$validators.semver = function (modelValue, viewValue) {
-            if (typeof viewValue !== 'string' || viewValue.length > 256) {
+            if (typeof viewValue !== 'string') {
+              return undefined;
+            }
+
+            var version = viewValue.trim();
+            if (version.length === 0) {
+              return undefined;
+            }
+
+            if (version.length > 256) {
               return false;
             }
 
             var regex = new RegExp('^v?(0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)(?:-((?:0|[1-9]\\d*|\\d*[a-zA-Z-][a-zA-Z0-9-]*)(?:\\.(?:0|[1-9]\\d*|\\d*[a-zA-Z-][a-zA-Z0-9-]*))*))?(?:\\+([0-9A-Za-z-]+(?:\\.[0-9A-Za-z-]+)*))?$');
-            return regex.test(viewValue);
+            return regex.test(version);
           };
         }
       };
