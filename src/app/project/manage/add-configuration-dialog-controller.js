455db6a2533c1410239881dbb720fd2df66c327f
Blake Niemyjski <bniemyjski@gmail.com>
2016-12-20 08:54:43 -0600

    Fixed an issue with adding configuration settings
    This had to do with an upgrade issue to angular 1.6

--- src/app/project/manage/add-configuration-dialog-controller.js
+++ src/app/project/manage/add-configuration-dialog-controller.js
@@ -2,7 +2,7 @@
   'use strict';
 
   angular.module('app.project')
-    .controller('AddConfigurationDialog', function ($ExceptionlessClient, $uibModalInstance, configuration) {
+    .controller('AddConfigurationDialog', function ($ExceptionlessClient, $uibModalInstance, data) {
       var vm = this;
       function cancel() {
         $ExceptionlessClient.submitFeatureUsage(vm._source + '.cancel');
@@ -21,7 +21,7 @@
       this.$onInit = function $onInit() {
         vm._source = 'app.project.AddConfigurationDialog';
         vm.cancel = cancel;
-        vm.configuration = configuration;
+        vm.configuration = data;
         vm.data = {};
         vm.save = save;
         $ExceptionlessClient.submitFeatureUsage(vm._source);
