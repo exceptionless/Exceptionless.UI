319df8ea411821a61c3cc77399c1d21404258c4a
Blake Niemyjski <bniemyjski@gmail.com>
2017-01-08 22:38:43 -0600

    Fixed stats issue

--- src/app/dashboard-controller.js
+++ src/app/dashboard-controller.js
@@ -34,17 +34,16 @@
 
           var dateAggregation = results.aggregations['date_date'].items || [];
           vm.chart.options.series[0].data = dateAggregation.map(function (item) {
-            var isFirstAggregation = item.aggregations['terms_is_first_occurrence'].items;
-            return {x: moment(item.key).unix(), y: isFirstAggregation ? isFirstAggregation[0].total || 0 : 0, data: item};
+            return {x: moment(item.key).unix(), y: item.aggregations['cardinality_stack_id'].value || 0, data: item};
           });
 
           vm.chart.options.series[1].data = dateAggregation.map(function (item) {
-            return {x: moment(item.key).unix(), y: item.aggregations['cardinality_stack_id'].value || 0, data: item};
+            return {x: moment(item.key).unix(), y: item.total || 0, data: item};
           });
         }
 
         var offset = filterService.getTimeOffset();
-        return eventService.count('date:(date' + (offset && '^' + offset) + ' cardinality:stack_id terms:(is_first_occurrence @include:true)) cardinality:stack_id terms:(is_first_occurrence @include:true)').then(onSuccess);
+        return eventService.count('date:(date' + (offset && '^' + offset) + ' cardinality:stack_id) cardinality:stack_id terms:(is_first_occurrence @include:true)').then(onSuccess);
       }
 
       function getOrganizations() {
