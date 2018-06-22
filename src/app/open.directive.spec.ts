a2cbc3d55bd3e949aee8387918724a4524eaa66d
ggoldong <dev.pros17@gmail.com>
2018-06-22 12:15:09 +0800

    remove spec files

--- src/app/open.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { OpenDirective } from './open.directive';
-
-describe('OpenDirective', () => {
-  it('should create an instance', () => {
-    const directive = new OpenDirective();
-    expect(directive).toBeTruthy();
-  });
-});
