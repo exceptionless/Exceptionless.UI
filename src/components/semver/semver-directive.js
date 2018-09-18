8ee2afc3b07086bcc1e126805a4f84fb4608fb82
Phyxion <7643972+Phyxion@users.noreply.github.com>
2018-09-18 15:30:04 +0200

    Update semver-directive.js
    Accept all version numbers.

--- src/components/semver/semver-directive.js
+++ src/components/semver/semver-directive.js
@@ -12,19 +12,34 @@
             if (!inputValue) {
               return inputValue;
             }
-
-            var r = /^(\d+)\.(\d+)\.(\d+)\.(\d+)$/;
-            if (!r.test(inputValue)) {
-              return inputValue;
+            
+            var z = /^(\d+)\.(\d+)\.?(\d+)?\.?(\d)?$/;
+            var result = '';
+            if (z.test(inputValue))
+            {
+              var z2 = /^(\d+)\.(\d+)$/;
+              if (z2.test(inputValue))
+                  result = inputValue.replace(z2, '$1.$2.0-0');
+              else 
+              {
+                var z3 = /^(\d+)\.(\d+)\.(\d+)$/;
+                if (z3.test(inputValue))
+                    result = inputValue.replace(z3, '$1.$2.$3-0');
+                else {
+                  var z4 = /^(\d+)\.(\d+)\.(\d+)\.(\d+)$/;
+                  if (z4.test(inputValue))
+                    result = inputValue.replace(z4, '$1.$2.$3-$4');                
+              }
+                
+              if (result !== '')
+              {
+                 modelCtrl.$setViewValue(result);
+                 modelCtrl.$render();
+                 return result;
+              }              
             }
-
-            // convert 4 part version to semver (1.2.3.4 to 1.2.3-4)
-            var transformedInput = inputValue.replace(r, '$1.$2.$3-$4');
-            if (transformedInput !== inputValue) {
-              modelCtrl.$setViewValue(transformedInput);
-              modelCtrl.$render();
-            }
-            return transformedInput;
+              
+            return inputValue;
           });
         }
       };
