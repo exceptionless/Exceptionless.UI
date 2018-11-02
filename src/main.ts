22e7bdeb321911c228e69a767e5807ffb18b7b82
ggoldong <dev.pros17@gmail.com>
2018-11-02 23:02:28 +0700

    update for enable prod mode

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { enableProdMode } from '@angular/core';
+
+enableProdMode();
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.log(err));
