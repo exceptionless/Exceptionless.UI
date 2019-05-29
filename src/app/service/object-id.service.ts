786fd4b1e2a4901b063e6ff90b9cbcf3e140aad2
Blake Niemyjski <bniemyjski@gmail.com>
2019-05-29 10:04:25 -0500

    Updated object id library.

--- src/app/service/object-id.service.ts
+++ src/app/service/object-id.service.ts
@@ -1,34 +1,26 @@
 import { Injectable } from "@angular/core";
-import * as  ObjectId from "objectid-js";
+import ObjectID from "bson-objectid";
 
 @Injectable({
-    providedIn: "root"
+  providedIn: "root"
 })
 
 export class ObjectIdService {
-    constructor() {}
+  constructor() {}
 
-    public create(id: string|number): ObjectId {
-        return new ObjectId(id);
+  public create(id: string): ObjectID {
+    return new ObjectID(id);
+  }
+
+  public isValid(id: string) {
+    return ObjectID.isValid(id);
+  }
+
+  public getDate(id: string): Date {
+    if (!this.isValid(id)) {
+        return undefined;
     }
 
-    public isValid(id: string|number|any) {
-        if (!id || !(typeof id === "number" || id instanceof Number) && id.length !== 12 && id.length !== 24) {
-            return false;
-        }
-
-        if ((typeof id === "string") && id.length === 24) {
-            return /^[0-9a-fA-F]{24}$/i.test(id);
-        }
-
-        return true;
-    }
-
-    public getDate(id: string|number): Date {
-        if (!this.isValid(id)) {
-            return undefined;
-        }
-
-        return this.create(id).getDate();
-    }
+    return new Date(this.create(id).getTimestamp());
+  }
 }
