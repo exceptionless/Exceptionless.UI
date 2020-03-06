f3b428f56b182c450a2f6cac3c9e46c1533a8917
Eric J. Smith <eric@codesmithtools.com>
2020-03-06 10:19:11 -0600

    Use github actions to build

--- src/grunt/task-configs/copy.js
+++ src/grunt/task-configs/copy.js
@@ -1,8 +1,7 @@
 module.exports = {
     main: {
         files: [
-            { src: [ 'app_data/**', 'app.config.js', 'web.config', '*.png', '*.ico', 'img/**/{*.png,*.jpg,*.ico}','lang/**' ], dest: 'dist/' },
-            { src: [ '../.deployment', '../deploy.sh' ], dest: 'dist/blah/' }
+            { src: [ 'app.config.js', '*.png', '*.ico', 'img/**/{*.png,*.jpg,*.ico}','lang/**' ], dest: 'dist/' }
         ]
     }
 };
